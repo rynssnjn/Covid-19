d70ee013ce746c2e47b0d4e5731343e1cccf7816
Rael San Juan <rynssnjn@gmail.com>
2020-03-29 13:51:16 +0800

    Initial Commit

---
+++ Covid-19/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Covid-19
+//
+//  Created by Rael San Juan on 3/29/20.
+//  Copyright © 2020 Rael San Juan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

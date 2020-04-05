8f0c4070335125c86cb01dc973ece343d8a53880
Rael San Juan <rynssnjn@gmail.com>
2020-04-05 17:01:04 +0800

    [theming-change]
    removed unused colors

--- Covid-19/Utilities/Enums/AppUI.swift
+++ Covid-19/Utilities/Enums/AppUI.swift
@@ -11,15 +11,10 @@ import UIKit
 public enum AppUI {
 
     public enum Color {
-        static let lightPurple: UIColor = UIColor(red: 189.0/255.0, green: 188.0/255.0, blue: 247.0/255.0, alpha: 1.0)
 
         static let green: UIColor = UIColor(red: 58.0/255.0, green: 171.0/255.0, blue: 57.0/255.0, alpha: 1.0)
 
         static let lightGray: UIColor = UIColor(white: 0.9529, alpha: 1.0)
-
-        static let lightOrange: UIColor = UIColor(red: 245.0/255.0, green: 122.0/255.0, blue: 0.0/255.0, alpha: 0.2)
-
-        static let brown: UIColor = UIColor(red: 165.0/255.0, green: 42.0/255.0, blue: 42.0/255.0, alpha: 1.0)
 
         static let violet: UIColor = UIColor(red: 149.0/255.0, green: 52.0/255.0, blue: 235.0/255.0, alpha: 1.0)
 
@@ -39,8 +34,6 @@ public enum AppUI {
         static let darkishBlue: UIColor = UIColor(red: 50.0/255.0, green: 150.0/255.0, blue: 250.0/255.0, alpha: 1.0)
 
         static let darkPink: UIColor = UIColor(red: 247/255.0, green: 50.0/255.0, blue: 250.0/255.0, alpha: 1.0)
-
-        static let darkNavyBlue: UIColor = UIColor(red: 3.0/255.0, green: 2.0/255.0, blue: 59.0/255.0, alpha: 1.0)
 
         static let primary: UIColor = UIColor(red: 52.0/255.0, green: 73.0/255.0, blue: 85.0/255.0, alpha: 1.0)
 

db85a877769dcd85c31c826ac0a7e0f1fe0e6f4e
Rael San Juan <rynssnjn@gmail.com>
2020-04-05 18:33:16 +0800

    [search-bar-bugs]
    fixed search bar bugs

--- Covid-19/CountryList/CountryListView.swift
+++ Covid-19/CountryList/CountryListView.swift
@@ -26,7 +26,7 @@ public final class CountryListView: KioView {
         searchBar.placeholder = "search_country".localized
         searchBar.barTintColor = AppUI.Color.darkPrimary
         searchBar.tintColor = UIColor.white
-//        searchBar.searchTextField.textColor = UIColor.white
+        searchBar.searchTextField.textColor = UIColor.white
         searchBar.showsCancelButton = true
 
         return searchBar

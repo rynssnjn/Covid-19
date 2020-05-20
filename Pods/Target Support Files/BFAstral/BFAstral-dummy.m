cc4ebfde192b937d1d28e2c05d0c5a085976c09d
Rael San Juan <rynssnjn@gmail.com>
2020-05-20 20:27:32 +0800

    removed pods folder

--- Pods/Target Support Files/BFAstral/BFAstral-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_BFAstral : NSObject
-@end
-@implementation PodsDummy_BFAstral
-@end
--- Pods/Target Support Files/RSJ/RSJ-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_RSJ : NSObject
-@end
-@implementation PodsDummy_RSJ
-@end
